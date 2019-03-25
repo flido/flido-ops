82106051365d9812a39fed9997b5efd0204f436c
Leonardo <leonardo.matayoshi@lymlab.com.br>
2019-03-25 00:23:44 +0000

    Docker files

---
+++ containers/docker/spring-mvn/web-perf/src/main/java/mtech/mastertech/webperf/WebPerfApplication.java
@@ -0,0 +1,13 @@
+package mtech.mastertech.webperf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPerfApplication.class, args);
+	}
+
+}
---
+++ containers/docker/spring-mvn/web-perf/src/test/java/mtech/mastertech/webperf/WebPerfApplicationTests.java
@@ -0,0 +1,16 @@
+package mtech.mastertech.webperf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebPerfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
